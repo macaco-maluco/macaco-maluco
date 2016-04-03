7d1bfd6dbd38b013a1d3c7e4c9d1b5d780d3d2b2
Paulo Ragonha <paulo@ragonha.me>
2016-04-03 20:06:10 +0200

    Add open-graph information 💩

--- src/index.html.js
+++ src/index.html.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { renderToString } from 'react-dom/server'
 import LandingPage from './components/landing-page'
 import games from './data/games'
+import image from 'file!./og-image.png'
 
 module.exports = function ({ htmlWebpackPlugin }) {
   return (`
@@ -11,6 +12,16 @@ module.exports = function ({ htmlWebpackPlugin }) {
         <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0" />
         <meta charset="UTF-8">
         <title>Macaco Maluco</title>
+
+        <meta name="description" content="Macaco Maluco - Games built on Hackthons" />
+        <meta property="og:title" content="Macaco Maluco" />
+        <meta property="og:description" content="Games built on Hackthons" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Macaco Maluco" />
+        <meta name="twitter:title" content="Macaco Maluco" />
+        <meta name="twitter:description" content="Games built on Hackthons" />
+        <meta property="og:image" content="${image}" />
+
       </head>
       <body>
         <div id="macaco-maluco">${renderToString(<LandingPage games={games}/>)}</div>
