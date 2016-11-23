3ab9e2d0c9901674d3cdeff6388a526e3f2746ee
Paulo Ragonha <paulo@ragonha.me>
2016-11-23 12:31:25 -0200

    Change label of hero action 🏹

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,7 +6,7 @@ export default () => {
       <h1>Macaco Maluco</h1>
       <p>Web-based games built on Hackathons!</p>
       <div className='hero-actions'>
-        <a href='#games'>Play Now!</a>
+        <a href='#games'>The games</a>
         <a href='https://github.com/macaco-maluco'>Source code</a>
       </div>
     </div>
